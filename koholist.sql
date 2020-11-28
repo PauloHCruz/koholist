cde22468814c6d25b55afa11b8404e5bc563d70c
Paulo Henrique dos Santos Cruz <51004216+PauloHCruz@users.noreply.github.com>
2020-11-28 19:16:34 -0300

    Update koholist.sql
    atualização com acréscimo da tabela de imagens

--- koholist.sql
+++ koholist.sql
@@ -7,3 +7,10 @@ nome varchar(40),
 email varchar(40),
 senha varchar(32)
 );
+
+create table imagens(
+id_imagem int AUTO_INCREMENT primary key,
+nome_imagem varchar(40),
+fk_id_usuario int,
+FOREIGN key(fk_id_usuario) REFERENCES usuario (id_usuario) ON DELETE CASCADE
+);
